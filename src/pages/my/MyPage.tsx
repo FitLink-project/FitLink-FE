00947128b59fede364524dc6593fa72746568bf7
oculo0204 <oculo0204@gmail.com>
2025-11-29 19:51:16 +0900

    feat: #10 BottomBar

---
+++ src/pages/my/MyPage.tsx
@@ -0,0 +1,8 @@
+// src/pages/my/MyPage.tsx
+export default function MyPage() {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <p className="text-base font-pretendard">My 페이지 (준비 중)</p>
+    </div>
+  );
+}
